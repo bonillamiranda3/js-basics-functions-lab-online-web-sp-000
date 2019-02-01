f58dce5338d9b663f21fd0741ce8e0a7fe857946
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-02-01 01:46:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ function distanceFromHqInFeet (street) {
 function distanceTravelledInFeet (start, end) {
   return Math.abs(start - end) * 264
 }
+
+function calculatesFarePrice (start, destination) {
+let number_feet = distanceTravelledInFeet(start, destination)
+if (number_feet <= 400) {
+  return 0
+}
+  else if (number_feet > 400 && number_feet <= 2000) {
+    return .02 * (number_feet - 400)
+  }
+  else if (number_feet > 2000 && number_feet <= 2500) {
+    return 25
+  }
+  else {
+    return 'cannot travel that far'
+  }
+}
