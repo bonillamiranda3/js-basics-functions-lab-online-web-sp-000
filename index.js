508e2f95b5fcfd5ef9931125ebface9a455a84d5
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-02-01 01:34:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function distanceFromHqInBlocks (street) {
   return Math.abs(42-street)
 }
 
-function distanceTravelledInFeet (street) {
-  return Math.abs(start - end) * 264
-}
-
 function distanceFromHqInFeet (street) {
   return distanceFromHqInBlocks(street) * 264
+}
+
+function distanceTravelledInFeet (street) {
+  return Math.abs(start - end) * 264
 }
