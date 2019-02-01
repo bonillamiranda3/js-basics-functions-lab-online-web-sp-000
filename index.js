0688489f7a5bbd3e87853e211a41eb4f5a040ee2
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-02-01 01:47:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks (street) {
+  return Math.abs(42-street)
+}
+
+function distanceFromHqInFeet (street) {
+  return distanceFromHqInBlocks(street) * 264
+}
+
+function distanceTravelledInFeet (start, end) {
+  return Math.abs(start - end) * 264
+}
+
+function calculatesFarePrice (start, destination) {
+let number_feet = distanceTravelledInFeet(start, destination)
+if (number_feet <= 400) {
+  return 0
+}
+  else if (number_feet > 400 && number_feet <= 2000) {
+    return .02 * (number_feet - 400)
+  }
+  else if (number_feet > 2000 && number_feet <= 2500) {
+    return 25
+  }
+  else {
+    return 'cannot travel that far'
+  }
+}
