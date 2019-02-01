e256919f908348cc0c688dd2fb9685b695015b7d
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-02-01 01:33:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks (street) {
+  return Math.abs(42-street)
+}
+
+function distanceTravelledInFeet (street) {
+  return Math.abs(start - end) * 264
+}
+
+function distanceFromHqInFeet (street) {
+  return distanceFromHqInBlocks(street) * 264
+}
